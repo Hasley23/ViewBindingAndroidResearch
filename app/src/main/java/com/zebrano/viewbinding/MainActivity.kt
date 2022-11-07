0d014af6e4f3f8ad611ee5e45648df7615abdd4a
Hasley23 <hasleyclone@gmail.com>
2022-11-07 23:49:12 +0300

    added some logic and strings

--- app/src/main/java/com/zebrano/viewbinding/MainActivity.kt
+++ app/src/main/java/com/zebrano/viewbinding/MainActivity.kt
@@ -2,6 +2,10 @@ package com.zebrano.viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatDelegate
 import com.zebrano.viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +16,21 @@ class MainActivity : AppCompatActivity() {
 
         // используем класс привязки к XML файлу
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
+
+        // Выключаем ночной режим
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         // указываем корень класса для отображения
         setContentView(bindingClass.root)
 
         // работаем с классом
-        bindingClass.tvMain.text = getText(R.string.tvMain)
+        bindingClass.tvMain.text = getText(R.string.tvMain) // имя приложения
         bindingClass.tvMain.textSize = 24F
+        bindingClass.bAction.isAllCaps = false // Выключаем ВОТ ТАКОЕ
+
+        // слушатель кнопки
+        bindingClass.bAction.setOnClickListener {
+            Log.d("AppLog","Button pushed!")
+        }
+
     }
 }
